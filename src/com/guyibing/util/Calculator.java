859ea9300df963347d6f5f22b229dbf33a5c5a14
dev01 <yibing_gu@365tfb.com>
2018-03-08 10:11:57 +0800

    edit project name

---
+++ src/com/guyibing/util/Calculator.java
@@ -0,0 +1,28 @@
+package com.guyibing.util;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+    
+    public int subtract(int a, int b){
+        return a - b;
+    }
+    
+    public int multiply(int a, int b){
+        return a * b;
+    }
+    
+    public int divide(int a, int b){
+        return a / b;
+    }
+    
+    public int square(int a){
+        return  a * a;
+    }
+    
+    public int clear(){
+        return 0;
+    }
+
+}
---
+++ test/com/guyibing/util/AllCalculatorTests.java
@@ -0,0 +1,18 @@
+package com.guyibing.util;
+
+
+
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({CalculatorTest.class, SquareTest1.class,SquareTest2.class})
+
+public class AllCalculatorTests{}
+
+
+ 
+
+
